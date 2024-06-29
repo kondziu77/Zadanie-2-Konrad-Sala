797b3210677becdef88bc370a2eacfc394bb1c5c
Your Name <you@example.com>
2024-06-29 08:36:27 +0200

    Dodaj pliki projektów.

---
+++ Zadanie 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class SortowanieLiczb
+{
+    private int[] liczby = new int[6];
+
+    public void read_data()
+    {
+        Console.WriteLine("Wprowadź 6 liczb do posortowania:");
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write($"Liczba {i + 1}: ");
+            liczby[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    public void process_data()
+    {
+        Array.Sort(liczby);
+    }
+
+    public void show_results()
+    {
+        Console.WriteLine("Posortowane liczby:");
+        foreach (int liczba in liczby)
+        {
+            Console.Write(liczba + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SortowanieLiczb sortowanie = new SortowanieLiczb();
+
+        sortowanie.read_data();
+        sortowanie.process_data();
+        sortowanie.show_results();
+    }
+}
